6aef7fca9ccae4d07751e56d17f4d442ef5a2beb
aaronluannguyen <aaronluannguyen@gmail.com>
2018-04-23 01:04:42 -0700

    Finetuned intro screen and added answer screen

---
+++ app/src/main/java/edu/wahingoton/nguyen51/quizdroid/Views/AnswerActivity.kt
@@ -0,0 +1,13 @@
+package edu.wahingoton.nguyen51.quizdroid.Views
+import edu.wahingoton.nguyen51.quizdroid.R
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AnswerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer)
+    }
+}
