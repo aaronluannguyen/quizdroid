8f91a84377055a6c0a37cf434b835dc0ab733050
aaronluannguyen <aaronluannguyen@gmail.com>
2018-05-03 23:18:59 -0700

    Created a class that extends android application and logs to diagnostics onCreate

---
+++ app/src/main/java/edu/wahingoton/nguyen51/quizdroid/QuizApp.kt
@@ -0,0 +1,11 @@
+package edu.wahingoton.nguyen51.quizdroid
+
+import android.app.Application
+import android.util.Log
+
+class QuizApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Log.i("QuizApp Status", "QuizApp loaded and running...")
+    }
+}
