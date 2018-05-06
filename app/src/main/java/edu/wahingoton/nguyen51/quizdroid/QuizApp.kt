7852e5bc277fc33ca53797077f4ecae3459d9f78
Student User <studentuser@Students-MacBook-Pro.local>
2018-05-05 22:47:46 -0700

    Finished making quiz app a singleton that holds access to all the quizzes

--- app/src/main/java/edu/wahingoton/nguyen51/quizdroid/QuizApp.kt
+++ app/src/main/java/edu/wahingoton/nguyen51/quizdroid/QuizApp.kt
@@ -6,12 +6,19 @@ import edu.wahingoton.nguyen51.quizdroid.Controller.marvel
 import edu.wahingoton.nguyen51.quizdroid.Controller.math
 import edu.wahingoton.nguyen51.quizdroid.Controller.physics
 
-object QuizApp : Application() {
+class QuizApp : Application() {
+    companion object {
+        val MathQuiz = TopicRepository.MathQuiz
+        val PhysicsQuiz = TopicRepository.PhysicsQuiz
+        val MarvelQuiz = TopicRepository.MarvelQuiz
+    }
+
     override fun onCreate() {
         super.onCreate()
         Log.i("QuizApp Status", "QuizApp loaded and running...")
     }
 
+    // Might not need this function
     fun getQuiz(name: String): TopicRepository.Quiz {
         when (name) {
             math -> return TopicRepository.MathQuiz
--- app/src/main/java/edu/wahingoton/nguyen51/quizdroid/Views/MainActivity.kt
+++ app/src/main/java/edu/wahingoton/nguyen51/quizdroid/Views/MainActivity.kt
@@ -3,7 +3,7 @@ package edu.wahingoton.nguyen51.quizdroid.Views
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import edu.wahingoton.nguyen51.quizdroid.Controller.*
+import edu.wahingoton.nguyen51.quizdroid.QuizApp
 import edu.wahingoton.nguyen51.quizdroid.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,20 +15,20 @@ class MainActivity : AppCompatActivity() {
 
         MathCard.setOnClickListener {
             val intent = Intent(this, QuizFragmentsActivity::class.java).apply {
-                putExtra("topic", getTopic(math))
+                putExtra("topic", QuizApp.MathQuiz)
             }
             startActivity(intent)
         }
 
         PhysicsCard.setOnClickListener {
             val intent = Intent(this, QuizFragmentsActivity::class.java).apply {
-                putExtra("topic", getTopic(physics))
+                putExtra("topic", QuizApp.PhysicsQuiz)
             }
             startActivity(intent)
         }
         MarvelCard.setOnClickListener {
             val intent = Intent(this, QuizFragmentsActivity::class.java).apply {
-                putExtra("topic", getTopic(marvel))
+                putExtra("topic", QuizApp.MarvelQuiz)
             }
             startActivity(intent)
         }
