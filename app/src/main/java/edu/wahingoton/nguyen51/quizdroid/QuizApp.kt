9d90cb61d5195b933eca7de459c727ec444b8a70
Student User <studentuser@Students-MacBook-Pro.local>
2018-05-09 04:53:51 -0700

    Saving progress for night. Need to debug context for file retrieval

--- app/src/main/java/edu/wahingoton/nguyen51/quizdroid/QuizApp.kt
+++ app/src/main/java/edu/wahingoton/nguyen51/quizdroid/QuizApp.kt
@@ -1,44 +1,14 @@
 package edu.wahingoton.nguyen51.quizdroid
 
 import android.app.Application
-import android.content.pm.PackageManager
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.util.Log
-import com.google.gson.Gson
-import edu.wahingoton.nguyen51.quizdroid.Controller.marvel
-import edu.wahingoton.nguyen51.quizdroid.Controller.math
-import edu.wahingoton.nguyen51.quizdroid.Controller.physics
-import edu.wahingoton.nguyen51.quizdroid.Views.MainActivity
-import java.io.File
 
-class QuizApp : Application() {
+class QuizApp: Application() {
     companion object {
-        val MathQuiz = TopicRepository.MathQuiz
-        val PhysicsQuiz = TopicRepository.PhysicsQuiz
-        val MarvelQuiz = TopicRepository.MarvelQuiz
+        fun quizzes() = getQuizzes()
     }
-
     override fun onCreate() {
         super.onCreate()
         Log.i("QuizApp Status", "QuizApp loaded and running...")
-
-        var file = File("./sdcard/questions.json")
-        if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity(), arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
-        }
-
-        val gson = Gson()
-        val quizzes: Array<JsonQuiz> = gson.fromJson(file.reader(), Array<JsonQuiz>::class.java)
-    }
-
-    // Might not need this function
-    fun getQuiz(name: String): TopicRepository.Quiz {
-        when (name) {
-            math -> return TopicRepository.MathQuiz
-            physics -> return TopicRepository.PhysicsQuiz
-            marvel -> return TopicRepository.MarvelQuiz
-        }
-        return null!!
     }
 }
--- app/src/main/java/edu/wahingoton/nguyen51/quizdroid/Views/MainActivity.kt
+++ app/src/main/java/edu/wahingoton/nguyen51/quizdroid/Views/MainActivity.kt
@@ -1,18 +1,12 @@
 package edu.wahingoton.nguyen51.quizdroid.Views
 
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
-import android.util.Log
-import com.google.gson.Gson
+import android.widget.ArrayAdapter
 import edu.wahingoton.nguyen51.quizdroid.JsonQuiz
 import edu.wahingoton.nguyen51.quizdroid.QuizApp
 import edu.wahingoton.nguyen51.quizdroid.R
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,37 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setMainView()
-
-        MathCard.setOnClickListener {
-            val intent = Intent(this, QuizFragmentsActivity::class.java).apply {
-                putExtra("topic", QuizApp.MathQuiz)
-            }
-            startActivity(intent)
+        val quizzes = QuizApp.quizzes()
+        val formatQuiz = arrayOf<String?>()
+        for (i in quizzes.indices) {
+            formatQuiz[i] = quizzes[i].title
         }
-
-        PhysicsCard.setOnClickListener {
-            val intent = Intent(this, QuizFragmentsActivity::class.java).apply {
-                putExtra("topic", QuizApp.PhysicsQuiz)
-            }
-            startActivity(intent)
-        }
-        MarvelCard.setOnClickListener {
-            val intent = Intent(this, QuizFragmentsActivity::class.java).apply {
-                putExtra("topic", QuizApp.MarvelQuiz)
-            }
-            startActivity(intent)
-        }
-    }
-
-    fun setMainView() {
-        MathTitle.setText(QuizApp.MathQuiz.title)
-        MathShortDesc.setText(QuizApp.MathQuiz.shortDescription)
-
-        PhysicsTitle.setText(QuizApp.PhysicsQuiz.title)
-        PhysicsShortDesc.setText(QuizApp.PhysicsQuiz.shortDescription)
-
-        MarvelTitle.setText(QuizApp.MarvelQuiz.title)
-        MarvelShortDesc.setText(QuizApp.MarvelQuiz.shortDescription)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, formatQuiz)
+        QuizList.adapter = adapter
     }
 }
