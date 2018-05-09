3e308c9e7e4d427611780c3b835ebafa67c24074
Student User <studentuser@D-10-19-84-18.dhcp4.washington.edu>
2018-05-09 16:24:21 -0700

    Refactored code into main activity

--- app/src/main/java/edu/wahingoton/nguyen51/quizdroid/Views/MainActivity.kt
+++ app/src/main/java/edu/wahingoton/nguyen51/quizdroid/Views/MainActivity.kt
@@ -1,12 +1,18 @@
 package edu.wahingoton.nguyen51.quizdroid.Views
 
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.widget.ArrayAdapter
+import com.google.gson.Gson
+import edu.wahingoton.nguyen51.quizdroid.JsonQuiz
 import edu.wahingoton.nguyen51.quizdroid.QuizApp
 import edu.wahingoton.nguyen51.quizdroid.R
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +20,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val quizzes = QuizApp.quizzes
+        var file = File("./sdcard/questions.json")
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        }
+
+        val gson = Gson()
+        val quizzes: Array<JsonQuiz> = gson.fromJson(file.reader(), Array<JsonQuiz>::class.java)
+        QuizApp.quizzes = quizzes
+
         val formatQuiz = arrayOfNulls<String?>(quizzes.size)
         for (i in quizzes.indices) {
             formatQuiz[i] = quizzes[i].title
