18dd9b0438417722c7a9dd0c66b2f24618a4e0f2
Student User <studentuser@Students-MacBook-Pro.local>
2018-05-12 03:32:30 -0700

    Finished debugging waiting for user response for permissions

--- app/src/main/java/edu/wahingoton/nguyen51/quizdroid/Views/MainActivity.kt
+++ app/src/main/java/edu/wahingoton/nguyen51/quizdroid/Views/MainActivity.kt
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
-import android.util.Log
-import android.widget.ArrayAdapter
 import com.google.gson.Gson
 import edu.wahingoton.nguyen51.quizdroid.JsonQuiz
 import edu.wahingoton.nguyen51.quizdroid.QuizApp
@@ -30,11 +28,25 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
-            }
+            getPermission()
+        } else {
+            startMain()
         }
 
+    }
+
+    fun getPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        startMain()
+    }
+
+    fun startMain() {
         var file = File("./sdcard/questions.json")
         val gson = Gson()
         val quizzes: Array<JsonQuiz> = gson.fromJson(file.reader(), Array<JsonQuiz>::class.java)
